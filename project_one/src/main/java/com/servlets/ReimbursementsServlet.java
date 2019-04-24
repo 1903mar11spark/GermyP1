83ea7a6bf353830be44b006b81f631369d1cfe51
Germy Louis <germylouis@gmail.com>
2019-04-24 12:02:22 -0400

    CC

--- project_one/src/main/java/com/servlets/ReimbursementsServlet.java
+++ project_one/src/main/java/com/servlets/ReimbursementsServlet.java
@@ -20,8 +20,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
@@ -72,10 +70,10 @@ public class ReimbursementsServlet extends HttpServlet {
 		HttpSession session = request.getSession(false);
 		/*Blobs: 1. Create an object of type Part, 2. Request to get a parameter that matches the name of an input from your HTML file.
 		 * For me, that parameter was "uploadFile". In the form that surrounds the upload button and other information relevant to Reimbursement
-		 * requests I have a value enctype="multipart/form-data" and a @MultipartConfig tag at the top my servlet. This may or may not be necessary.
-		 * 3. Create InputStream obj, then set it equal to InputStreamObject.getInputStream(). 4. Send InmputStream Object to DAO.
-		 * 5. In your DAO set your parameter in your SQL query as setBlob such like this smt.setBlob(1, req.getImg());
-		 * Note: getImage method is of type InputStream and is a part of my Reimbursement Request Class.*/
+		 * requests I have a value enctype="multipart/form-data" and a @MultipartConfig tag at the top my Servlet. This may or may not be necessary.
+		 * 3. Create InputStream obj, then set that equal to PartObject.getInputStream(). 4. Send InmputStream Object to DAO.
+		 * 5. In your DAO, set your parameter in your SQL query as setBlob such like this smt.setBlob(1, req.getImg());
+		 * Note: getImage method is of type InputStream and is a part of my Reimbursement Request Class. Large files may take up to 20 seconds to load.*/
 		
 		Part filePart = request.getPart("uploadFile");
 		InputStream fileContent = null;
@@ -84,7 +82,7 @@ public class ReimbursementsServlet extends HttpServlet {
 		if(filePart != null) {
          fileContent = filePart.getInputStream();
 		
-		}else {System.out.println("File empty");}
+		
 		
 		String amount = request.getParameter("amount");
 		String cat = request.getParameter("category");
@@ -92,13 +90,14 @@ public class ReimbursementsServlet extends HttpServlet {
 		String first = session.getAttribute("First").toString();
 		String last = session.getAttribute("Last").toString();
 		String email = session.getAttribute("Email").toString();
-		String mID = session.getAttribute("Manager").toString();
 		String eID = session.getAttribute("Employee").toString();
 	
+		if(session.getAttribute("Manager") == null) {
+			
+				
 		
-		if(fileContent != null) {
 			Integer newid = Integer.parseInt(eID);
-			Integer newmanager = Integer.parseInt(mID);
+			Integer newmanager = 0;
 			Double newamount = Double.parseDouble(amount);
 			
 			ReimbursementsReq req = new ReimbursementsReq(fileContent,newamount,newid,first,last,email,newmanager,script,cat);
@@ -107,28 +106,71 @@ public class ReimbursementsServlet extends HttpServlet {
 						reim.submitRequestWithImage(req);
 						response.sendRedirect("ProfileServlet");
 					} catch (SQLException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 			
 			}else{
+					String mID = session.getAttribute("Manager").toString();
 					Integer newid = Integer.parseInt(eID);
 					Integer newmanager = Integer.parseInt(mID);
 					Double newamount = Double.parseDouble(amount);
 					
-					ReimbursementsReq req = new ReimbursementsReq(newamount,newid,first,last,email,newmanager,script,cat);
+					ReimbursementsReq req = new ReimbursementsReq(fileContent,newamount,newid,first,last,email,newmanager,script,cat);
 					
 					try {
 						reim.submitRequest(req);
 						response.sendRedirect("ProfileServlet");
 					} catch (SQLException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 			}
+		}else {
+			System.out.println("File empty");
+			String amount = request.getParameter("amount");
+			String cat = request.getParameter("category");
+			String script = request.getParameter("description");
+			String first = session.getAttribute("First").toString();
+			String last = session.getAttribute("Last").toString();
+			String email = session.getAttribute("Email").toString();
+			String eID = session.getAttribute("Employee").toString();
 		
-		
+			if(session.getAttribute("Manager") == null) {
+				
+					
+			
+				Integer newid = Integer.parseInt(eID);
+				Integer newmanager = 0;
+				Double newamount = Double.parseDouble(amount);
+				
+				ReimbursementsReq req = new ReimbursementsReq(newamount,newid,first,last,email,newmanager,script,cat);
+				
+						try {
+							reim.submitRequestWithImage(req);
+							response.sendRedirect("ProfileServlet");
+						} catch (SQLException e) {
+							e.printStackTrace();
+						}
+				
+				}else{
+						String mID = session.getAttribute("Manager").toString();
+						Integer newid = Integer.parseInt(eID);
+						Integer newmanager = Integer.parseInt(mID);
+						Double newamount = Double.parseDouble(amount);
+						
+						ReimbursementsReq req = new ReimbursementsReq(newamount,newid,first,last,email,newmanager,script,cat);
+						
+						try {
+							reim.submitRequest(req);
+							response.sendRedirect("ProfileServlet");
+						} catch (SQLException e) {
+							e.printStackTrace();
+						}
+			
+			}
+		}
+	}
+}	
 		
 
-	}
-}
+	
+
